20007eb085251b92f800bbda321741c464be0317
Sampada-VS <samssvv1@gmail.com>
2020-10-21 19:12:48 +0530

    Adding calculate daily wage for part time feature

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,18 +1,20 @@
 
 public class EmployeeWage {
-		public static final int IS_FULL_TIME=1;
-		public static final int RATE_PER_HOUR=20;
+	public static final int IS_FULL_TIME=1;
+	public static final int IS_PART_TIME=2;
+	public static final int RATE_PER_HOUR=20;
 
-		public static void main(String[] args)
-		{
-			int empWage=0;
-			int empHrs=0;
+	public static void main(String[] args)
+	{
+		int empWage=0;
+		int empHrs=0;
 
-			int empCheck=(int)((Math.random()*10)%2);
-			if(empCheck == IS_FULL_TIME)
-				empHrs=8;
-			empWage=empHrs*RATE_PER_HOUR;
-			System.out.println("Employee daily wage: "+empWage);
-		}
-
+		int empCheck=(int)((Math.random()*10)%3);
+		if(empCheck == IS_FULL_TIME)
+			empHrs=8;
+		else if(empCheck == IS_PART_TIME)
+			empHrs=4;
+		empWage=empHrs*RATE_PER_HOUR;
+		System.out.println("Employee daily wage: "+empWage);
+	}
 }
