b9235f8bb261345a015089ae0e9c9a5535f6e793
Sampada-VS <samssvv1@gmail.com>
2020-10-21 19:08:41 +0530

    Adding calculate daily wage feature

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,13 +1,18 @@
 
 public class EmployeeWage {
+		public static final int IS_FULL_TIME=1;
+		public static final int RATE_PER_HOUR=20;
 
-	public static void main(String[] args) {
-		int IS_FULL_TIME=1;
-		int empCheck=(int)((Math.random()*10)%2);
-		if(empCheck == IS_FULL_TIME)
-			System.out.println("Employee is Present.");
-		else
-			System.out.println("Employee is Absent.");
+		public static void main(String[] args)
+		{
+			int empWage=0;
+			int empHrs=0;
+
+			int empCheck=(int)((Math.random()*10)%2);
+			if(empCheck == IS_FULL_TIME)
+				empHrs=8;
+			empWage=empHrs*RATE_PER_HOUR;
+			System.out.println("Employee daily wage: "+empWage);
 		}
 
 }
