2d619d5a337f7a255d00498ea7c2ffb96b54c0c8
Sampada-VS <samssvv1@gmail.com>
2020-10-21 19:01:18 +0530

    Adding checking attendance feature

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,7 +2,12 @@
 public class EmployeeWage {
 
 	public static void main(String[] args) {
-			System.out.println("Welcome to Employee Wage Computation Problem.");
-	}
+		int IS_FULL_TIME=1;
+		int empCheck=(int)((Math.random()*10)%2);
+		if(empCheck == IS_FULL_TIME)
+			System.out.println("Employee is Present.");
+		else
+			System.out.println("Employee is Absent.");
+		}
 
 }
