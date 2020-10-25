e59b4dc01f283e6f69223f47e5baf0cd5f171565
Sampada-VS <samssvv1@gmail.com>
2020-10-25 17:07:37 +0530

    Refactoring- wage for multiple companies using objects

--- src/com/employeewage/EmployeeWage.java
+++ src/com/employeewage/EmployeeWage.java
@@ -4,36 +4,52 @@ public class EmployeeWage {
 	public static final int IS_FULL_TIME=1;
 	public static final int IS_PART_TIME=2;
 
+	public final String company;
+	public final int empRatePerHr;
+	public final int numOfWorkingDays;
+	public final int maxHrs;
+
+	public EmployeeWage(String company, int empRatePerHr, int numOfWorkingDays, int maxHrs)
+	{
+		this.company=company;
+		this.empRatePerHr=empRatePerHr;
+		this.numOfWorkingDays=numOfWorkingDays;
+		this.maxHrs=maxHrs;
+	}
+
 	public static void main(String[] args)
 	{
-		calculateWage("Reliance",15,5,10);
-		calculateWage("TCS",10,2,10);
+		EmployeeWage deloitte=new EmployeeWage("Deloitte",20,2,10);
+		EmployeeWage microsoft=new EmployeeWage("Microsoft",30,5,30);
+		deloitte.calculateWage();
+		microsoft.calculateWage();
+
 	}
-	public static void calculateWage(String company, int ratePerHr, int numOfworkingDays, int maxHrs)
+	public void calculateWage()
 	{
-		int empWage;
 		int empHrs;
-		int totalEmpWage=0;
+		int empWage;
 		int totalEmpHours=0;
 		int totalWorkingDays=0;
-		System.out.println("For company "+company+" : ");
-		while (totalEmpHours<maxHrs && totalWorkingDays<numOfworkingDays)
+		System.out.println("For company "+company+"======");
+		while (totalEmpHours<maxHrs && totalWorkingDays<numOfWorkingDays)
 		{
 			totalWorkingDays++;
-			int empCheck=(int)((Math.random()*10)%3);
-			switch(empCheck)
+			int empCheck = (int)((Math.random()*10)%3);
+			switch (empCheck)
 			{
-				case IS_FULL_TIME:
+				case IS_FULL_TIME :
 					empHrs=8;
 					break;
-				case IS_PART_TIME:
+				case IS_PART_TIME :
 					empHrs=4;
 					break;
-				default:
+				default :
 					empHrs=0;
 			}
+
 			totalEmpHours=totalEmpHours+empHrs;
-			if(totalEmpHours>maxHrs)
+			if (totalEmpHours>maxHrs)
 			{
 				totalEmpHours=totalEmpHours-empHrs;
 				totalWorkingDays--;
@@ -41,13 +57,13 @@ public class EmployeeWage {
 			}
 			else
 			{
-				empWage=empHrs*ratePerHr;
-				System.out.println("On day "+totalWorkingDays+" == Employee Daily wage: "+empWage);
+				empWage=empHrs*empRatePerHr;
+				System.out.println("Employee daily wage :"+empWage+" Total Employee Hours : "+totalEmpHours);
 			}
 		}
-		totalEmpWage=totalEmpHours*ratePerHr;
-		System.out.println("Total Employee Wage :"+totalEmpWage);
-		System.out.println("Total Employee Hours : "+totalEmpHours);
-		System.out.println("Total working days : "+totalWorkingDays);
+		int totalEmpWage=totalEmpHours*empRatePerHr;
+		System.out.println("Total employee wage :"+totalEmpWage);
+		System.out.println("Total employee hours :"+totalEmpHours);
+		System.out.println("Total working days :"+totalWorkingDays);
 	}
 }
